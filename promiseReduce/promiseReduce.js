efc5c162874d14e3ec7270a37a163a286c26ca8e
artem <magnus_0991@bk.ru>
2021-07-17 16:42:45 +0300

    Reformat

---
+++ promiseReduce/promiseReduce.js
@@ -0,0 +1,33 @@
+var fn1 = () => {
+	console.log('fn1')
+	return Promise.resolve(1)
+}
+
+var fn2 = () => new Promise(resolve => {
+	console.log('fn2')
+	setTimeout(() => resolve(2), 1000)
+})
+
+function promiseReduce(asyncFunctions, reduce, initialValue) {
+	/*  
+	 * Реализация
+	 */
+}
+
+/**
+fn1
+reduce
+fn2
+reduce
+2
+
+ */
+promiseReduce(
+	[fn1, fn2],
+	function (memo, value) {
+		console.log('reduce')
+		return memo * value
+	},
+	1
+)
+	.then(console.log)
